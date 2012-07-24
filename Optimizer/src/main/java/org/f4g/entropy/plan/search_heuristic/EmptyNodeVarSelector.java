0d2d14b941ab5950e3b0545f6e6f26d9bee1969f
Corentin Dupont <cdupont@cdupont-laptop.(none)>
2012-07-24 17:17:02 +0200

    empty node selector that select the energy inefficient nodes first (to switch them off)

--- Optimizer/src/main/java/org/f4g/entropy/plan/search_heuristic/EmptyNodeVarSelector.java
+++ Optimizer/src/main/java/org/f4g/entropy/plan/search_heuristic/EmptyNodeVarSelector.java
@@ -3,21 +3,24 @@
 package org.f4g.entropy.plan.search_heuristic;
 
 
+import java.util.Collections;
+
 import choco.kernel.solver.search.integer.AbstractIntVarSelector;
 import choco.kernel.solver.variables.integer.IntDomainVar;
+import entropy.configuration.Configuration;
 import entropy.configuration.ManagedElementSet;
 import entropy.configuration.Node;
 import entropy.plan.choco.ReconfigurationProblem;
 import entropy.plan.choco.actionModel.ManageableNodeActionModel;
 
 import org.apache.log4j.Logger;
+import org.f4g.entropy.configuration.F4GNodeComparator;
+import org.f4g.entropy.configuration.F4GResourcePicker;
 
 /**
- * A Var selector that focuses on the assignment var of the demanding slices.
- * To improve the process, it is possible to desactivate the scheduling constraints
- * at the beginning of the heuristic. However, don't forget to activate them at the end.
+ * A Var selector that select the empty, energy inefficient nodes first
  *
- * @author Fabien Hermenier
+ * @author Corentin Dupont
  */
 public class EmptyNodeVarSelector extends AbstractIntVarSelector {
 
@@ -32,7 +35,7 @@ public class EmptyNodeVarSelector extends AbstractIntVarSelector {
      * By default, the heuristic doesn't touch the scheduling constraints.
      *
      * @param solver the solver to use to extract the assignment variables
-     * @param slices the slices to consider
+     * @param nodes the nodes to consider
      */
     public EmptyNodeVarSelector(ReconfigurationProblem myPb, ManagedElementSet<Node> myNodes) {
     	super(myPb);
@@ -40,20 +43,26 @@ public class EmptyNodeVarSelector extends AbstractIntVarSelector {
 		pb = myPb;
 		nodes = myNodes;
 		
+		//first criteria: select first the servers that are energy inefficient to switch them off
+        F4GNodeComparator cmp = new F4GNodeComparator(false, F4GResourcePicker.NodeRc.powerIdle, (Configuration) myPb.getSourceConfiguration());
+  
+        Collections.sort(nodes, cmp);
+		
     }
     
-    //select the shutdown for idle nodes
+    //select the node state variable in order
     @Override
     public IntDomainVar selectVar() {
-    
+        	
 	    for(int i = 0; i < nodes.size(); i++) {
 	    	Node n = nodes.get(i);
 		   
+	    	//future online and future offline should have their state set already
 	        if(!pb.getFutureOnlines().contains(n) && !pb.getFutureOfflines().contains(n)) {
 	        	ManageableNodeActionModel action = (ManageableNodeActionModel) pb.getAssociatedAction(n);
-	        	//log.debug("action.getState().isInstantiated():" + action.getState().isInstantiated());
-	        	if(!action.getState().isInstantiated() && pb.getSourceConfiguration().isOnline(n)) {
-	        		//log.debug("switch off node:" + n.getName());
+
+	        	//Select the empty nodes first
+	        	if(!action.getState().isInstantiated() && pb.getUsedMem(n).isInstantiated() && (pb.getUsedMem(n).getVal() == 0) ) {
 	        		return action.getState();
 	        	}
 	        }    
