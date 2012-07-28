5e918e0eca5d053fd3a436f921460e19e2ec4b12
Fabien Hermenier <hermenierfabien@gmail.com>
2012-07-28 13:14:01 +0200

    - new ant target "bench-dist" to generate a standalone application for benchmarking - shell scripts to launch benchmark FIXME: fix issue with ModelGenerator that have the path to the Schema hard coded

---
+++ Optimizer/src/main/bin/benchmark_daemon.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+MY=`dirname $0`
+
+JAVA_OPTS="-Xmx4G -Xms4G -server -da"
+#Define the classpath
+JARS=`ls $MY/lib/*.jar`
+ 
+for JAR in $JARS; do
+ CLASSPATH=$JAR:$CLASSPATH
+done
+java $JAVA_OPTS -cp $CLASSPATH org.f4g.optimizer.BenchServer $*
--- Optimizer/src/main/java/org/f4g/optimizer/BenchClient.java
+++ Optimizer/src/main/java/org/f4g/optimizer/BenchClient.java
@@ -3,11 +3,10 @@ package org.f4g.optimizer;
 import entropy.jobsManager.Job;
 import entropy.jobsManager.JobHandler;
 import org.f4g.optimizer.CloudTraditional.SLAReader;
+import org.f4g.optimizer.utils.Utils;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.List;
 
 /**
  *  A client for BenchServer.
@@ -54,6 +53,7 @@ public class BenchClient {
 
     }
     public static void main(String [] args) {
+        Utils.initLogger("config/log4j-benchmark.properties");
         if (args.length == 0) {
             usage(1);
         }
