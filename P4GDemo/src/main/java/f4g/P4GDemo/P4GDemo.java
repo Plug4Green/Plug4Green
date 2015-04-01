2846cd4fafcaaa962a792309f61b00624f5fc1a9
Michele Santuari <michele.santuari@create-net.org>
2015-04-01 15:26:04 +0200

    Small fix

--- P4GDemo/src/main/java/f4g/P4GDemo/P4GDemo.java
+++ P4GDemo/src/main/java/f4g/P4GDemo/P4GDemo.java
@@ -1,5 +1,6 @@
 package f4g.P4GDemo;
 
+import java.io.File;
 import java.util.HashMap;
 
 import f4g.com.openstack.ComOpenstack;
@@ -15,9 +16,17 @@ public class P4GDemo {
 	
 	ComOpenstack comOS = new ComOpenstack();
 	HashMap<String, ICom> map = new HashMap<String, ICom>();
+	File f4gProp = new File("./f4gconfig.properties");
+
 	map.put("comOpenstack", comOS);
 	Main p4g = new Main(map);
-	p4g.init("src/main/config/core/f4gconfig.properties");
-	p4g.startup();
+	if(f4gProp.exists()) {
+		p4g.init(f4gProp.getAbsolutePath());
+		p4g.startup();
+    	} else {
+		p4g.init("src/main/config/core/f4gconfig.properties");
+		p4g.startup();
+    	}
+
     }
 }
