6aff3d4a37b09eb08dacf593e956eb75ae03b392
Corentin Dupont <cdupont@create-net.org>
2015-08-26 10:35:28 +0200

    new ConfigWriter class to serialize to YAML

--- Core/src/main/java/f4g/optimizer/ConfigWriter.java
+++ Core/src/main/java/f4g/optimizer/ConfigWriter.java
@@ -1,10 +1,8 @@
 package f4g.optimizer;
 
-
 import f4g.schemas.java.metamodel.FIT4Green;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.representer.Representer;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -16,7 +14,6 @@ public class ConfigWriter {
 
 	public ConfigWriter() {
 	}	
-
 
     public static void writeYaml(String fileName, Object o) {
 
@@ -42,19 +39,10 @@ public class ConfigWriter {
         }
     }
 
-
-    //Extension to Snake YAML to serialize/deserialize iCals & JScience
-    public static class CalRepresenter extends Representer {
-        public CalRepresenter() {
-
-        }
-
-    }    
-
     //for debugging purpose
     public static void writeConfig(FIT4Green f4g) {
 
-        writeYaml("test.yaml", f4g);
+        writeYaml("FIT4Green.yaml", f4g);
     }
 
 }
